68a71ea4e80edea84fcb1b349f0b20b38eda8362
vladstojna <vlad.leic@gmail.com>
2021-08-31 21:29:08 +0100

    implement dgels and sgels workloads

--- lapacke/lapacke.cpp
+++ lapacke/lapacke.cpp
@@ -15,76 +15,117 @@
 
 namespace
 {
-    auto get_gen(std::mt19937_64& engine, std::uniform_real_distribution<double>& dist)
+    namespace detail
     {
-        return [&]()
+        auto get_gen(std::mt19937_64& engine, std::uniform_real_distribution<double>& dist)
         {
-            return dist(engine);
-        };
-    }
+            return [&]()
+            {
+                return dist(engine);
+            };
+        }
 
-    void handle_error(int res)
-    {
-        if (res > 0)
-            throw std::logic_error("The solution could not be computed");
-        if (res < 0)
-            throw std::runtime_error("Error during computation");
-    }
+        void handle_error(int res)
+        {
+            if (res > 0)
+                throw std::logic_error("The solution could not be computed");
+            if (res < 0)
+                throw std::runtime_error("Error during computation");
+        }
 
-    __attribute__((noinline)) void dgesv(std::size_t N,
-        std::size_t Nrhs,
-        std::mt19937_64& engine,
-        std::uniform_real_distribution<double>& dist)
-    {
-        std::vector<double> a(N * N);
-        std::vector<double> b(N * Nrhs);
-        std::vector<lapack_int> ipiv(N);
+        template<typename Real, typename Func>
+        void gesv_impl(
+            Func func,
+            std::size_t N,
+            std::size_t Nrhs,
+            std::mt19937_64& engine,
+            std::uniform_real_distribution<double>& dist)
+        {
+            std::vector<Real> a(N * N);
+            std::vector<Real> b(N * Nrhs);
+            std::vector<lapack_int> ipiv(N);
 
-        auto gen = get_gen(engine, dist);
-        std::generate(a.begin(), a.end(), gen);
-        std::generate(b.begin(), b.end(), gen);
+            auto gen = get_gen(engine, dist);
+            std::generate(a.begin(), a.end(), gen);
+            std::generate(b.begin(), b.end(), gen);
 
-        tp::printer tpr;
-        int res = LAPACKE_dgesv(
-            LAPACK_ROW_MAJOR, N, Nrhs,
-            a.data(), N,
-            ipiv.data(),
-            b.data(), Nrhs);
+            tp::printer tpr;
+            int res = func(LAPACK_ROW_MAJOR, N, Nrhs, a.data(), N, ipiv.data(), b.data(), Nrhs);
+            handle_error(res);
+        }
 
-        handle_error(res);
-    }
+        template<typename Real, typename Func>
+        void gels_impl(
+            Func func,
+            std::size_t M,
+            std::size_t N,
+            std::size_t Nrhs,
+            std::mt19937_64& engine,
+            std::uniform_real_distribution<double>& dist)
+        {
+            std::vector<Real> a(M * N);
+            std::vector<Real> b(std::max(N, M) * Nrhs);
 
-    __attribute__((noinline)) void sgesv(std::size_t N,
-        std::size_t Nrhs,
-        std::mt19937_64& engine,
-        std::uniform_real_distribution<double>& dist)
-    {
-        std::vector<float> a(N * N);
-        std::vector<float> b(N * Nrhs);
-        std::vector<lapack_int> ipiv(N);
+            auto gen = get_gen(engine, dist);
+            std::generate(a.begin(), a.end(), gen);
+            std::generate(b.begin(), b.end(), gen);
 
-        auto gen = get_gen(engine, dist);
-        std::generate(a.begin(), a.end(), gen);
-        std::generate(b.begin(), b.end(), gen);
-
-        tp::printer tpr;
-        int res = LAPACKE_sgesv(
-            LAPACK_ROW_MAJOR, N, Nrhs,
-            a.data(), N,
-            ipiv.data(),
-            b.data(), Nrhs);
-
-        handle_error(res);
+            tp::printer tpr;
+            int res = func(LAPACK_ROW_MAJOR, 'N', M, N, Nrhs, a.data(), N, b.data(), Nrhs);
+            handle_error(res);
+        }
     }
 
     using work_func = void(*)(
         std::size_t,
         std::size_t,
+        std::size_t,
         std::mt19937_64&,
         std::uniform_real_distribution<double>&);
 
+    __attribute__((noinline)) void dgesv(
+        std::size_t,
+        std::size_t N,
+        std::size_t Nrhs,
+        std::mt19937_64& engine,
+        std::uniform_real_distribution<double>& dist)
+    {
+        detail::gesv_impl<double>(LAPACKE_dgesv, N, Nrhs, engine, dist);
+    }
+
+    __attribute__((noinline)) void sgesv(
+        std::size_t,
+        std::size_t N,
+        std::size_t Nrhs,
+        std::mt19937_64& engine,
+        std::uniform_real_distribution<double>& dist)
+    {
+        detail::gesv_impl<float>(LAPACKE_sgesv, N, Nrhs, engine, dist);
+    }
+
+    __attribute__((noinline)) void dgels(
+        std::size_t M,
+        std::size_t N,
+        std::size_t Nrhs,
+        std::mt19937_64& engine,
+        std::uniform_real_distribution<double>& dist)
+    {
+        detail::gels_impl<double>(LAPACKE_dgels, M, N, Nrhs, engine, dist);
+    }
+
+    __attribute__((noinline)) void sgels(
+        std::size_t M,
+        std::size_t N,
+        std::size_t Nrhs,
+        std::mt19937_64& engine,
+        std::uniform_real_distribution<double>& dist)
+    {
+        detail::gels_impl<float>(LAPACKE_sgels, M, N, Nrhs, engine, dist);
+    }
+
     class cmdparams
     {
+        std::size_t m = 0;
         std::size_t n = 0;
         std::size_t nrhs = 0;
         work_func func = nullptr;
@@ -92,10 +133,10 @@ namespace
     public:
         cmdparams(int argc, const char* const* argv)
         {
-            if (argc < 4)
+            if (argc < 2)
             {
                 print_usage(argv[0]);
-                throw std::invalid_argument("Not enough arguments");
+                throw std::invalid_argument("Too few arguments");
             }
             std::string op_type = argv[1];
             std::transform(op_type.begin(), op_type.end(), op_type.begin(),
@@ -104,12 +145,28 @@ namespace
                     return std::tolower(c);
                 });
 
-            util::to_scalar(argv[2], n);
-            util::to_scalar(argv[3], nrhs);
-            if (op_type == "dgesv")
-                func = dgesv;
-            else if (op_type == "sgesv")
-                func = sgesv;
+            func = get_work_func(op_type);
+            if (func == dgels || func == sgels)
+            {
+                if (argc < 5)
+                {
+                    print_usage(argv[0]);
+                    throw std::invalid_argument(op_type.append(": Too few arguments"));
+                }
+                util::to_scalar(argv[2], m);
+                util::to_scalar(argv[3], n);
+                util::to_scalar(argv[4], nrhs);
+            }
+            else if (func == dgesv || func == sgesv)
+            {
+                if (argc < 4)
+                {
+                    print_usage(argv[0]);
+                    throw std::invalid_argument(op_type.append(": Too few arguments"));
+                }
+                util::to_scalar(argv[2], n);
+                util::to_scalar(argv[3], nrhs);
+            }
             else
             {
                 print_usage(argv[0]);
@@ -122,13 +179,28 @@ namespace
             std::mt19937_64& engine,
             std::uniform_real_distribution<double>& dist)
         {
-            func(n, nrhs, engine, dist);
+            func(m, n, nrhs, engine, dist);
         }
 
     private:
+        work_func get_work_func(const std::string& str)
+        {
+            if (str == "dgels")
+                return dgels;
+            if (str == "dgesv")
+                return dgesv;
+            if (str == "sgels")
+                return sgels;
+            if (str == "sgesv")
+                return sgesv;
+            return nullptr;
+        }
+
         void print_usage(const char* prog)
         {
-            std::cerr << "Usage: " << prog << " {dgesv,sgesv} <n> <nrhs>\n";
+            std::cerr << "Usage:\n"
+                << "\t" << prog << " {dgesv,sgesv} <n> <nrhs>\n"
+                << "\t" << prog << " {dgels,sgels} <m> <n> <nrhs>\n";
         }
     };
 }
