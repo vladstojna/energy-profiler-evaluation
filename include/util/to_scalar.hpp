907e71d879f96c8aa1dd9e0cc57516fb9a56f948
Vlad Stojna <vlad.leic@gmail.com>
2021-09-02 17:32:21 +0100

    Specialize to_scalar for FP types if no compiler support (#9)
    * implement specializations
    
    * change to c++ locale facilities

--- include/util/to_scalar.hpp
+++ include/util/to_scalar.hpp
@@ -3,6 +3,11 @@
 #include <charconv>
 #include <string_view>
 
+#if __GNUC__ < 11
+#include <cstdlib>
+#include <locale>
+#endif // __GNUC__ < 11
+
 namespace util
 {
     template<typename T>
@@ -13,6 +18,48 @@ namespace util
         if (auto code = std::make_error_code(ec))
             throw std::system_error(code);
     }
+
+    // specialize floating point conversions if GCC version < 11
+#if __GNUC__ < 11
+    namespace detail
+    {
+        template<typename Real, typename Func>
+        void to_scalar_impl(std::string_view str, Real& value, Func func)
+        {
+            std::string tmp(str);
+            std::locale loc;
+            std::locale::global(std::locale::classic());
+            errno = 0;
+            Real val = func(tmp.c_str(), nullptr);
+            if (errno)
+            {
+                std::locale::global(loc);
+                throw std::system_error(
+                    std::error_code(static_cast<int>(errno), std::generic_category()));
+            }
+            std::locale::global(loc);
+            value = val;
+        }
+    }
+
+    template<>
+    void to_scalar(std::string_view str, float& value)
+    {
+        detail::to_scalar_impl<float>(str, value, std::strtof);
+    }
+
+    template<>
+    void to_scalar(std::string_view str, double& value)
+    {
+        detail::to_scalar_impl<double>(str, value, std::strtod);
+    }
+
+    template<>
+    void to_scalar(std::string_view str, long double& value)
+    {
+        detail::to_scalar_impl<long double>(str, value, std::strtold);
+    }
+#endif // __GNUC__ < 11
 
     template<typename T>
     T to_scalar(std::string_view str)
