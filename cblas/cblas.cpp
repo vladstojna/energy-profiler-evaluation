4dd40dcf4464261ad6bf25bf1d67b418afe44f3b
vladstojna <vlad.leic@gmail.com>
2021-09-04 14:34:53 +0100

    impl template for code reuse, explicit sample after filling matrices

--- cblas/cblas.cpp
+++ cblas/cblas.cpp
@@ -15,110 +15,86 @@
 
 namespace
 {
-    auto get_gen(std::mt19937_64& engine, std::uniform_real_distribution<double>& dist)
+    using work_func = void(*)(
+        std::size_t,
+        std::size_t,
+        std::size_t,
+        std::mt19937_64&);
+
+    namespace detail
     {
-        return [&]()
+        template<typename Real, CBLAS_TRANSPOSE Trans, typename Func>
+        void gemm_impl(Func func, std::size_t M, std::size_t N, std::size_t K, std::mt19937_64& engine)
         {
-            return dist(engine);
-        };
+            std::uniform_real_distribution<Real> dist{ 0.0, 1.0 };
+            auto gen = [&]() { return dist(engine); };
+
+            tp::printer tpr;
+            std::vector<Real> a(M * K);
+            std::vector<Real> b(K * N);
+            std::vector<Real> c(M * N);
+            std::generate(a.begin(), a.end(), gen);
+            std::generate(b.begin(), b.end(), gen);
+
+            tpr.sample();
+            func(CblasRowMajor, CblasNoTrans, Trans,
+                M, N, K, 1.0,
+                a.data(), K,
+                b.data(), N,
+                0, c.data(), N
+            );
+        }
     }
 
     __attribute__((noinline)) void dgemm_no_transpose(
         std::size_t M,
         std::size_t N,
         std::size_t K,
-        std::mt19937_64& engine,
-        std::uniform_real_distribution<double>& dist)
+        std::mt19937_64& engine)
     {
-        auto gen = get_gen(engine, dist);
-        std::vector<double> a(M * K);
-        std::vector<double> b(K * N);
-        std::vector<double> c(M * N);
-        std::generate(a.begin(), a.end(), gen);
-        std::generate(b.begin(), b.end(), gen);
-
-        tp::printer tpr;
-        cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
-            M, N, K, 1.0,
-            a.data(), K,
-            b.data(), N,
-            0, c.data(), N
-        );
+        return detail::gemm_impl<double, CblasNoTrans>(cblas_dgemm, M, N, K, engine);
     }
 
     __attribute__((noinline)) void dgemm(
         std::size_t M,
         std::size_t N,
         std::size_t K,
-        std::mt19937_64& engine,
-        std::uniform_real_distribution<double>& dist)
+        std::mt19937_64& engine)
     {
-        std::vector<double> a(M * K);
-        std::vector<double> b(K * N);
-        std::vector<double> c(M * N);
-        auto gen = get_gen(engine, dist);
-        std::generate(a.begin(), a.end(), gen);
-        std::generate(b.begin(), b.end(), gen);
-
-        tp::printer tpr;
-        cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasTrans,
-            M, N, K, 1.0,
-            a.data(), K,
-            b.data(), K,
-            0, c.data(), N
-        );
-    }
-
-    __attribute__((noinline)) void dgemv(
-        std::size_t M,
-        std::size_t N,
-        std::size_t,
-        std::mt19937_64& engine,
-        std::uniform_real_distribution<double>& dist)
-    {
-        std::vector<double> a(M * N);
-        std::vector<double> x(N);
-        std::vector<double> y(M);
-        auto gen = get_gen(engine, dist);
-        std::generate(a.begin(), a.end(), gen);
-
-        tp::printer tpr;
-        cblas_dgemv(CblasRowMajor, CblasNoTrans,
-            M, N, 1.0,
-            a.data(), N,
-            x.data(), 1,
-            0, y.data(), 1);
+        return detail::gemm_impl<double, CblasTrans>(cblas_dgemm, M, N, K, engine);
     }
 
     __attribute__((noinline)) void sgemm(
         std::size_t M,
         std::size_t N,
         std::size_t K,
-        std::mt19937_64& engine,
-        std::uniform_real_distribution<double>& dist)
+        std::mt19937_64& engine)
     {
-        std::vector<float> a(M * K);
-        std::vector<float> b(K * N);
-        std::vector<float> c(M * N);
-        auto gen = get_gen(engine, dist);
-        std::generate(a.begin(), a.end(), gen);
-        std::generate(b.begin(), b.end(), gen);
-
-        tp::printer tpr;
-        cblas_sgemm(CblasRowMajor, CblasNoTrans, CblasTrans,
-            M, N, K, 1.0,
-            a.data(), K,
-            b.data(), K,
-            0, c.data(), N
-        );
+        return detail::gemm_impl<float, CblasTrans>(cblas_sgemm, M, N, K, engine);
     }
 
-    using work_func = void(*)(
+    __attribute__((noinline)) void dgemv(
+        std::size_t M,
+        std::size_t N,
         std::size_t,
-        std::size_t,
-        std::size_t,
-        std::mt19937_64&,
-        std::uniform_real_distribution<double>&);
+        std::mt19937_64& engine)
+    {
+        std::uniform_real_distribution<double> dist{ 0.0, 1.0 };
+        auto gen = [&]() { return dist(engine); };
+
+        tp::printer tpr;
+        std::vector<double> a(M * N);
+        std::vector<double> x(N);
+        std::vector<double> y(M);
+        std::generate(a.begin(), a.end(), gen);
+
+        tpr.sample();
+        cblas_dgemv(CblasRowMajor, CblasNoTrans,
+            M, N, 1.0,
+            a.data(), N,
+            x.data(), 1,
+            0, y.data(), 1);
+    }
 
     class cmdparams
     {
@@ -169,11 +145,9 @@ namespace
             assert(func);
         }
 
-        void do_work(
-            std::mt19937_64& engine,
-            std::uniform_real_distribution<double>& dist)
+        void do_work(std::mt19937_64& engine)
         {
-            func(m, n, k, engine, dist);
+            func(m, n, k, engine);
         }
 
     private:
@@ -189,11 +163,10 @@ int main(int argc, char** argv)
 {
     std::random_device rnd_dev;
     std::mt19937_64 engine{ rnd_dev() };
-    std::uniform_real_distribution dist{ 0.0, 1.0 };
     try
     {
         cmdparams params(argc, argv);
-        params.do_work(engine, dist);
+        params.do_work(engine);
     }
     catch (const std::exception& e)
     {
