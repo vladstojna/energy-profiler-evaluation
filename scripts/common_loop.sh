7e952a33e2583607f22e6cc424bdda7f8ffdbe1b
vladstojna <vlad.leic@gmail.com>
2021-09-05 10:52:05 +0100

    echo script name and iteration for clarity

--- scripts/common_loop.sh
+++ scripts/common_loop.sh
@@ -4,7 +4,7 @@ for i in $(seq 0 $(($iters - 1))); do
     if is_candidate $what $sample_name; then
         cmd="$samples_dir/$sample_name/$sample_name.out 20000"
         out="$outdir/$sample_name-20000.$i"
-        echo "$cmd"
+        echo "$0: i=$i $cmd"
         execute_command "$sample_name" "$cmd" "$out"
     fi
 
@@ -14,18 +14,18 @@ for i in $(seq 0 $(($iters - 1))); do
 
         cmd="$samples_dir/$sample_name/$sample_name.out 20000000000 0.0 1.0"
         out="$outdir/$sample_name-2e10_0_1-smton.$i"
-        echo "$cmd; threads=$OMP_NUM_THREADS"
+        echo "$0: i=$i $cmd; threads=$OMP_NUM_THREADS"
         execute_command "$sample_name" "$cmd" "$out"
 
         export OMP_NUM_THREADS="$cores"
         out="$outdir/$sample_name-2e10_0_1-smtoff.$i"
-        echo "$cmd; threads=$OMP_NUM_THREADS"
+        echo "$0: i=$i $cmd; threads=$OMP_NUM_THREADS"
         execute_command "$sample_name" "$cmd" "$out"
 
         export OMP_NUM_THREADS=1
         cmd="$samples_dir/$sample_name/$sample_name.out 2000000000 0.0 1.0"
         out="$outdir/$sample_name-2e9_0_1-singlethread.$i"
-        echo "$cmd; threads=$OMP_NUM_THREADS"
+        echo "$0: i=$i $cmd; threads=$OMP_NUM_THREADS"
         execute_command "$sample_name" "$cmd" "$out"
 
         unset OMP_NUM_THREADS
@@ -37,17 +37,17 @@ for i in $(seq 0 $(($iters - 1))); do
 
         cmd="$samples_dir/$sample_name/$sample_name.out 500 20"
         out="$outdir/$sample_name-500_20-smton.$i"
-        echo "$cmd"
+        echo "$0: i=$i $cmd"
         execute_command "$sample_name" "$cmd" "$out"
 
         cmd="$samples_dir/$sample_name/$sample_name.out 250 40"
         out="$outdir/$sample_name-250_40-smton.$i"
-        echo "$cmd"
+        echo "$0: i=$i $cmd"
         execute_command "$sample_name" "$cmd" "$out"
 
         cmd="$samples_dir/$sample_name/$sample_name.out 50 200"
         out="$outdir/$sample_name-50_200-smton.$i"
-        echo "$cmd"
+        echo "$0: i=$i $cmd"
         execute_command "$sample_name" "$cmd" "$out"
 
         unset OMP_NUM_THREADS
@@ -62,44 +62,44 @@ for i in $(seq 0 $(($iters - 1))); do
 
         cmd="$samples_dir/$sample_name/$sample_name-$pref.out dgemm 16000 16000 16000"
         out="$outdir/$pref-dgemm_16K_16K_16K-$suff"
-        echo "$cmd; threads=$OPENBLAS_NUM_THREADS"
+        echo "$0: i=$i $cmd; threads=$OPENBLAS_NUM_THREADS"
         execute_command "$sample_name" "$cmd" "$out"
 
         cmd="$samples_dir/$sample_name/$sample_name-$pref.out sgemm 20000 20000 20000"
         out="$outdir/$pref-sgemm_20K_20K_20K-$suff"
-        echo "$cmd; threads=$OPENBLAS_NUM_THREADS"
+        echo "$0: i=$i $cmd; threads=$OPENBLAS_NUM_THREADS"
         execute_command "$sample_name" "$cmd" "$out"
 
         sample_name="lapacke"
 
         cmd="$samples_dir/$sample_name/$sample_name-$pref.out dgesv 20000 100"
         out="$outdir/$pref-dgesv_20K_100-$suff"
-        echo "$cmd; threads=$OPENBLAS_NUM_THREADS"
+        echo "$0: i=$i $cmd; threads=$OPENBLAS_NUM_THREADS"
         execute_command "$sample_name" "$cmd" "$out"
 
         cmd="$samples_dir/$sample_name/$sample_name-$pref.out sgesv 26000 100"
         out="$outdir/$pref-sgesv_26K_100-$suff"
-        echo "$cmd; threads=$OPENBLAS_NUM_THREADS"
+        echo "$0: i=$i $cmd; threads=$OPENBLAS_NUM_THREADS"
         execute_command "$sample_name" "$cmd" "$out"
 
         cmd="$samples_dir/$sample_name/$sample_name-$pref.out dgels 10000 12000 100"
         out="$outdir/$pref-dgels_10K_12K_100-$suff"
-        echo "$cmd; threads=$OPENBLAS_NUM_THREADS"
+        echo "$0: i=$i $cmd; threads=$OPENBLAS_NUM_THREADS"
         execute_command "$sample_name" "$cmd" "$out"
 
         cmd="$samples_dir/$sample_name/$sample_name-$pref.out dgels 12000 10000 100"
         out="$outdir/$pref-dgels_12K_10K_100-$suff"
-        echo "$cmd; threads=$OPENBLAS_NUM_THREADS"
+        echo "$0: i=$i $cmd; threads=$OPENBLAS_NUM_THREADS"
         execute_command "$sample_name" "$cmd" "$out"
 
         cmd="$samples_dir/$sample_name/$sample_name-$pref.out sgels 12000 16000 100"
         out="$outdir/$pref-sgels_12K_16K_100-$suff"
-        echo "$cmd; threads=$OPENBLAS_NUM_THREADS"
+        echo "$0: i=$i $cmd; threads=$OPENBLAS_NUM_THREADS"
         execute_command "$sample_name" "$cmd" "$out"
 
         cmd="$samples_dir/$sample_name/$sample_name-$pref.out sgels 16000 12000 100"
         out="$outdir/$pref-sgels_16K_12K_100-$suff"
-        echo "$cmd; threads=$OPENBLAS_NUM_THREADS"
+        echo "$0: i=$i $cmd; threads=$OPENBLAS_NUM_THREADS"
         execute_command "$sample_name" "$cmd" "$out"
 
         unset OPENBLAS_NUM_THREADS
@@ -112,44 +112,44 @@ for i in $(seq 0 $(($iters - 1))); do
 
         cmd="$samples_dir/$sample_name/$sample_name-intel-mkl.out dgemm 16000 16000 16000"
         out="$outdir/$pref-dgemm_16K_16K_16K-$suff"
-        echo "$cmd"
+        echo "$0: i=$i $cmd"
         execute_command "$sample_name" "$cmd" "$out"
 
         cmd="$samples_dir/$sample_name/$sample_name-intel-mkl.out sgemm 20000 20000 20000"
         out="$outdir/$pref-sgemm_20K_20K_20K-$suff"
-        echo "$cmd"
+        echo "$0: i=$i $cmd"
         execute_command "$sample_name" "$cmd" "$out"
 
         sample_name="lapacke"
 
         cmd="$samples_dir/$sample_name/$sample_name-intel-mkl.out dgesv 22000 100"
         out="$outdir/$pref-dgesv_22K_100-$suff"
-        echo "$cmd"
+        echo "$0: i=$i $cmd"
         execute_command "$sample_name" "$cmd" "$out"
 
         cmd="$samples_dir/$sample_name/$sample_name-intel-mkl.out sgesv 28000 100"
         out="$outdir/$pref-sgesv_28K_100-$suff"
-        echo "$cmd"
+        echo "$0: i=$i $cmd"
         execute_command "$sample_name" "$cmd" "$out"
 
         cmd="$samples_dir/$sample_name/$sample_name-intel-mkl.out dgels 15000 20000 100"
         out="$outdir/$pref-dgels_15K_20K_100-$suff"
-        echo "$cmd"
+        echo "$0: i=$i $cmd"
         execute_command "$sample_name" "$cmd" "$out"
 
         cmd="$samples_dir/$sample_name/$sample_name-intel-mkl.out dgels 20000 15000 100"
         out="$outdir/$pref-dgels_20K_15K_100-$suff"
-        echo "$cmd"
+        echo "$0: i=$i $cmd"
         execute_command "$sample_name" "$cmd" "$out"
 
         cmd="$samples_dir/$sample_name/$sample_name-intel-mkl.out sgels 20000 24000 100"
         out="$outdir/$pref-sgels_20K_24K_100-$suff"
-        echo "$cmd"
+        echo "$0: i=$i $cmd"
         execute_command "$sample_name" "$cmd" "$out"
 
         cmd="$samples_dir/$sample_name/$sample_name-intel-mkl.out sgels 24000 20000 100"
         out="$outdir/$pref-sgels_24K_20K_100-$suff"
-        echo "$cmd"
+        echo "$0: i=$i $cmd"
         execute_command "$sample_name" "$cmd" "$out"
     fi
 done
