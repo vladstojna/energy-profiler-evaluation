0adabbc74e7590e63088779282cda06c623de2e7
vladstojna <vlad.leic@gmail.com>
2021-09-24 10:01:28 +0100

    add creation/destruction of cuSOLVERDn handle

---
+++ cusolver/cusolver.cpp
@@ -0,0 +1,45 @@
+#include <timeprinter/printer.hpp>
+#include <util/cuda_utils.hpp>
+#include <util/to_scalar.hpp>
+#include <util/unique_handle.hpp>
+
+#include <cusolverDn.h>
+
+namespace
+{
+    tp::printer g_tpr;
+
+    template<auto Func>
+    struct func_obj : std::integral_constant<decltype(Func), Func> {};
+
+    cusolverDnHandle_t cusolverdn_create()
+    {
+        cusolverDnHandle_t handle;
+        auto status = cusolverDnCreate(&handle);
+        if (status != CUSOLVER_STATUS_SUCCESS)
+            throw std::runtime_error("Error creating cuSOLVER");
+        return handle;
+    }
+
+    void cusolverdn_destroy(cusolverDnHandle_t handle)
+    {
+        auto status = cusolverDnDestroy(handle);
+        if (status != CUSOLVER_STATUS_SUCCESS)
+            std::cerr << "Error destroying cuSOLVER";
+    }
+
+    using cusolverdn_handle =
+        util::unique_handle<cusolverDnHandle_t, func_obj<cusolverdn_destroy>>;
+}
+
+int main(int argc, char** argv)
+{
+    try
+    {
+        cusolverdn_handle handle{ cusolverdn_create() };
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}
