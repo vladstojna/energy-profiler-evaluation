9c34c31f998b97e185c3241c6c5c1b5e7177958f
Vlad Stojna <vlad.leic@gmail.com>
2021-09-26 22:01:54 +0100

    Port cuBLAS sample to new util classes (#30)
    * port cuBLAS sample using new buffer classes
    
    * switch from get/set matrix to copy
    
    * rename caller
    
    * port cublas_xt using unique handle and callers
    
    * remove host buffer
    
    * remove default template parameter
    
    * remove matrix A and B copies

--- cublas/cublas.cpp
+++ cublas/cublas.cpp
@@ -1,103 +1,50 @@
+#include <timeprinter/printer.hpp>
+#include <util/to_scalar.hpp>
+#include <util/cuda_utils.hpp>
+#include <util/unique_handle.hpp>
+
 #include <cublas_v2.h>
 #include <cuda_runtime_api.h>
 #include <cuda.h>
 
-#include <timeprinter/printer.hpp>
-#include <util/to_scalar.hpp>
-
 #include <algorithm>
 #include <cassert>
-#include <charconv>
 #include <random>
-#include <vector>
 
 namespace
 {
     tp::printer g_tpr;
 
-    class cublas_handle
+    template<auto Func>
+    struct func_obj : std::integral_constant<decltype(Func), Func> {};
+
+    cublasHandle_t cublas_create()
     {
-        cublasHandle_t _handle;
+        cublasHandle_t handle;
+        auto status = cublasCreate(&handle);
+        if (status != CUBLAS_STATUS_SUCCESS)
+            throw std::runtime_error("Error creating cuBLAS");
+        return handle;
+    }
 
-    public:
-        cublas_handle() :
-            _handle(nullptr)
-        {
-            if (cublasStatus_t ret = cublasCreate(&_handle); ret != CUBLAS_STATUS_SUCCESS)
-                throw std::runtime_error("Error instantiating cuBLAS");
-        }
-
-        ~cublas_handle()
-        {
-            if (cublasStatus_t ret = cublasDestroy(_handle); ret != CUBLAS_STATUS_SUCCESS)
-                std::cerr << "Error destroying cuBLAS\n";
-        }
-
-        cublasHandle_t get()
-        {
-            return _handle;
-        }
-
-        operator cublasHandle_t ()
-        {
-            return get();
-        }
-    };
-
-    template<typename T>
-    class device_buffer
+    void cublas_destroy(cublasHandle_t handle)
     {
-    public:
-        using value_type = T;
+        auto status = cublasDestroy(handle);
+        if (status != CUBLAS_STATUS_SUCCESS)
+            std::cerr << "Error destroying cuBLAS\n";
+    }
 
-    public:
-        device_buffer(std::size_t size)
-        {
-            auto res = cudaMalloc(reinterpret_cast<void**>(&_ptr), size * sizeof(value_type));
-            if (res != cudaSuccess)
-                throw std::runtime_error(cudaGetErrorString(res));
-        }
-
-        ~device_buffer()
-        {
-            auto res = cudaFree(_ptr);
-            if (res != cudaSuccess)
-                std::cerr << "Error freeing device memory: " << cudaGetErrorString(res) << "\n";
-        }
-
-        const T* get() const
-        {
-            return _ptr;
-        }
-
-        T* get()
-        {
-            return _ptr;
-        }
-
-        operator const T* () const
-        {
-            return get();
-        }
-
-        operator T* ()
-        {
-            return get();
-        }
-
-    private:
-        T* _ptr;
-    };
-
-    using work_func = void(*)(
-        std::size_t,
-        std::size_t,
-        std::size_t,
-        cublasHandle_t,
-        std::mt19937_64&);
+    using cublas_handle = util::unique_handle<cublasHandle_t, func_obj<cublas_destroy>>;
 
     namespace detail
     {
+        template<typename>
+        struct gemm_caller {};
+        template<>
+        struct gemm_caller<float> : func_obj<cublasSgemm> {};
+        template<>
+        struct gemm_caller<double> : func_obj<cublasDgemm> {};
+
         void handle_error(cublasStatus_t status)
         {
             if (status == CUBLAS_STATUS_SUCCESS)
@@ -117,82 +64,46 @@ namespace
             }
         }
 
-        template<typename Real, typename Func>
+        template<typename Real>
         void gemm_impl(
-            Func func,
             std::size_t M,
             std::size_t N,
             std::size_t K,
-            cublasHandle_t handle,
+            cublas_handle& handle,
             std::mt19937_64& engine)
         {
             std::uniform_real_distribution<Real> dist{ 0.0, 1.0 };
             auto gen = [&]() { return dist(engine); };
 
             tp::sampler smp(g_tpr);
-            Real alpha = 1;
-            Real beta = 0;
-            std::vector<Real> a(M * K);
-            std::vector<Real> b(K * N);
-            std::vector<Real> c(M * N);
+            Real alpha = 1.0;
+            Real beta = 0.0;
+            util::buffer<Real> a{ M * K };
+            util::buffer<Real> b{ K * N };
+            util::buffer<Real> c{ M * N };
             std::generate(a.begin(), a.end(), gen);
             std::generate(b.begin(), b.end(), gen);
 
             smp.do_sample();
-            device_buffer<Real> dev_a(a.size());
-            device_buffer<Real> dev_b(b.size());
-            device_buffer<Real> dev_c(c.size());
-            auto res = cublasSetMatrix(
-                M, K, sizeof(typename decltype(a)::value_type),
-                a.data(), M,
-                dev_a.get(), M);
-            if (res != CUBLAS_STATUS_SUCCESS)
-                throw std::runtime_error("Error setting matrix A");
-            res = cublasSetMatrix(
-                K, N, sizeof(typename decltype(b)::value_type),
-                b.data(), K,
-                dev_b.get(), K);
-            if (res != CUBLAS_STATUS_SUCCESS)
-                throw std::runtime_error("Error setting matrix B");
-            res = cublasSetMatrix(
-                M, N, sizeof(typename decltype(c)::value_type),
-                c.data(), M,
-                dev_c.get(), M);
-            if (res != CUBLAS_STATUS_SUCCESS)
-                throw std::runtime_error("Error setting matrix C");
+            util::device_buffer dev_a{ a.begin(), a.end() };
+            util::device_buffer dev_b{ b.begin(), b.end() };
+            util::device_buffer<Real> dev_c{ c.size() };
 
             smp.do_sample();
-            res = func(
+            auto res = gemm_caller<Real>::value(
                 handle,
                 CUBLAS_OP_N,
                 CUBLAS_OP_N,
                 M, N, K, &alpha,
-                dev_a, M,
-                dev_b, K,
+                dev_a.get(), M,
+                dev_b.get(), K,
                 &beta,
-                dev_c, M);
+                dev_c.get(), M);
             handle_error(res);
             cudaDeviceSynchronize();
 
             smp.do_sample();
-            res = cublasGetMatrix(
-                M, K, sizeof(typename decltype(dev_a)::value_type),
-                dev_a.get(), M,
-                a.data(), M);
-            if (res != CUBLAS_STATUS_SUCCESS)
-                throw std::runtime_error("Error getting matrix A");
-            res = cublasGetMatrix(
-                K, N, sizeof(typename decltype(dev_b)::value_type),
-                dev_b.get(), K,
-                b.data(), K);
-            if (res != CUBLAS_STATUS_SUCCESS)
-                throw std::runtime_error("Error getting matrix B");
-            res = cublasGetMatrix(
-                M, N, sizeof(typename decltype(dev_c)::value_type),
-                dev_c.get(), M,
-                c.data(), M);
-            if (res != CUBLAS_STATUS_SUCCESS)
-                throw std::runtime_error("Error getting matrix C");
+            util::copy(dev_c, dev_c.size(), c.begin());
         }
     }
 
@@ -200,24 +111,25 @@ namespace
         std::size_t M,
         std::size_t N,
         std::size_t K,
-        cublasHandle_t handle,
+        cublas_handle& handle,
         std::mt19937_64& engine)
     {
-        detail::gemm_impl<double>(cublasDgemm, M, N, K, handle, engine);
+        detail::gemm_impl<double>(M, N, K, handle, engine);
     }
 
     __attribute__((noinline)) void sgemm(
         std::size_t M,
         std::size_t N,
         std::size_t K,
-        cublasHandle_t handle,
+        cublas_handle& handle,
         std::mt19937_64& engine)
     {
-        detail::gemm_impl<float>(cublasSgemm, M, N, K, handle, engine);
+        detail::gemm_impl<float>(M, N, K, handle, engine);
     }
 
     struct cmdargs
     {
+        using work_func = decltype(&sgemm);
         std::size_t m = 0;
         std::size_t n = 0;
         std::size_t k = 0;
@@ -250,7 +162,7 @@ namespace
             assert(func);
         }
 
-        void do_work(cublasHandle_t handle, std::mt19937_64& engine) const
+        void do_work(cublas_handle& handle, std::mt19937_64& engine) const
         {
             func(m, n, k, handle, engine);
         }
@@ -267,10 +179,10 @@ int main(int argc, char** argv)
 {
     try
     {
-        cmdargs args(argc, argv);
+        const cmdargs args(argc, argv);
         std::random_device rnd_dev;
         std::mt19937_64 engine{ rnd_dev() };
-        cublas_handle handle;
+        cublas_handle handle{ cublas_create() };
         args.do_work(handle, engine);
     }
     catch (const std::exception& e)
